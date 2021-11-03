82e8bcc26ca58e5e03968741c3ceeb5513f84d56
GabeDesign27 <76018794+GabeDesign27@users.noreply.github.com>
2021-11-02 21:54:33 -0400

    Update random_num.py

--- random_num.py
+++ random_num.py
@@ -6,5 +6,5 @@ my_float = random.random()
 # One way to make random floats over 1.0... but always ends up being a factor of 10
 print(str(my_int) + str(my_float))
 
-# better way to make random floats:
+# better way to make random floats (This will make a float between 0 and 10:
 print(my_float * 10)
