30c8f065853330b5463dd7c63fead4b563c5c466
GabeDesign27 <76018794+GabeDesign27@users.noreply.github.com>
2021-11-02 21:56:34 -0400

    Update treasure_map.py

--- treasure_map.py
+++ treasure_map.py
@@ -1,13 +1,11 @@
-# 🚨 Don't change the code below 👇
 row1 = ["⬜️","⬜️","⬜️"]
 row2 = ["⬜️","⬜️","⬜️"]
 row3 = ["⬜️","⬜️","⬜️"]
 map = [row1, row2, row3]
 print(f"{row1}\n{row2}\n{row3}")
 position = input("Where do you want to put the treasure? ")
-# 🚨 Don't change the code above 👆
+
 my_list = []
-# Write your code below this row 👇
 for num in position:
   my_list.append(num)
 col = int(my_list[0])
@@ -17,10 +15,5 @@ row -= 1
 
 sel_row = map[row]
 sel_row[col] = "X"
-# print(col)
-# print(row)
 
-# Write your code above this row 👆
-
-# 🚨 Don't change the code below 👇
 print(f"{row1}\n{row2}\n{row3}")
